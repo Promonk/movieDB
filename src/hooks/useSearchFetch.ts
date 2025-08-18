8935b353457314f00b42ca37400fc2bd4357feb6
CraigMckee <craig.mckee@petplace.com>
2025-08-18 10:55:56 -0400

    modified:   src/components/Header.tsx

--- src/hooks/useSearchFetch.ts
+++ src/hooks/useSearchFetch.ts
@@ -7,7 +7,7 @@ export const useSearchFetch = (api: string, queryTerm: string | null) => {
 
   useEffect(()=> {
     const fetchMovies = async () => {
-        fetch(`https://api.themoviedb.org/3/${api}?query=${queryTerm}&include_adult=true&language=en-US&page=1`, getOptions)
+        fetch(`https://api.themoviedb.org/3/${api}?query=${queryTerm}&include_adult=false&language=en-US&page=1`, getOptions)
             .then(res => res.json())
             .then(res => setData(res.results))
             .catch(err => console.error(err));
